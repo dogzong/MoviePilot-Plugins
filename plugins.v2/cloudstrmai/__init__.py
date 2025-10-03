9c0ca62c2b385213bb7fbda614d520cd33c813a9
dogzong <chom2733@gmail.com>
2025-10-03 16:33:47 +0800

    🔖 更新插件版本到1.0.3
    - 完善插件属性：plugin_config_prefix、plugin_order、auth_level
    - 修复插件在MoviePilot UI中不显示的问题
    - 确保符合官方插件库标准
    - 更新版本历史记录

--- plugins.v2/cloudstrmai/__init__.py
+++ plugins.v2/cloudstrmai/__init__.py
@@ -397,7 +397,7 @@ class CloudStrmAI(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/thsrite/MoviePilot-Plugins/main/icons/create.png"
     # 插件版本
-    plugin_version = "1.0.2"
+    plugin_version = "1.0.3"
     # 插件作者
     plugin_author = "dogzong"
     # 作者主页
